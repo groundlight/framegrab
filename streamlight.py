8954f6d543b11d90c2f719fbebb7d771019d0d74
cesar romero <cesar@positronix.ai>
2022-06-09 12:14:32 -0700

    implements the --fps flag using multiple threads (#3)
    * implements then --fps flag
    * measure the time between captured frames to try to guarantee fps and warn whenever it looks like we're not keeping up
    * introduces worker threads that process frames they get a sync queue in which the main thread puts captured frames

--- streamlight.py
+++ streamlight.py
@@ -6,7 +6,7 @@ usage: streamlight [options] -t TOKEN -d DETECTOR
 options:
   -d, --detector=ID      detector id to which the image queries are sent
   -e, --endpoint=URL     api endpoint [default: https://device.positronix.ai/device-api]
-  -f, --framerate=FPS    number of frames to capture per second.
+  -f, --fps=FPS          number of frames to capture per second. [default: 5]
   -h, --help             show this message.
   -s, --stream=STREAM    id, filename or URL of a video stream (e.g. rtsp://host:port/script?params) [default: 0]
   -t, --token=TOKEN      api token to authenticate with the groundlight api
@@ -16,7 +16,9 @@ import io
 import logging
 from logging.config  import dictConfig
 import os
+from queue import Queue
 import time
+from threading import Thread
 
 import cv2
 import docopt
@@ -32,6 +34,27 @@ logger = logging.getLogger(name='groundlight.stream')
 INTEG = "https://device.integ.positronix.ai/device-api"
 
 
+def frame_processor(q:Queue, client:Groundlight, detector:str):
+    logger.debug(f'frame_processor({q=}, {client=}, {detector=})')
+    while True:
+       frame = q.get() # locks
+       # prepare image
+       start = time.time()
+       logger.debug(f"Original {frame.shape=}")
+       frame = cv2.resize(frame, (480,270))
+       logger.debug(f"Resized {frame.shape=}")
+       is_success, buffer = cv2.imencode(".jpg", frame)
+       logger.debug(f"buffer size is {len(buffer)}")
+       io_buf = io.BytesIO(buffer)
+       end = time.time()
+       logger.info(f"Prepared the image in {1000*(end-start):.1f}ms")
+       # send image query
+       image_query = client.submit_image_query(detector_id=detector, image=io_buf)
+       start = end
+       end = time.time()
+       logger.info(f"API time for image {1000*(end-start):.1f}ms")
+
+
 def main():
     args = docopt.docopt(__doc__)
     if args.get('--verbose'):
@@ -45,36 +68,52 @@ def main():
     DETECTOR = args['--detector']
     STREAM = args['--stream']
     try:
-        STREAM=int(STREAM)
+        STREAM = int(STREAM)
     except ValueError as e:
         logger.debug(f'{STREAM=} is not an int, so it must be a filename or url.')
+    FPS = args['--fps']
+    try:
+       FPS = float(FPS)
+       logger.debug(f'{FPS=}')
+    except ValueError as e:
+       logger.error(f'Invalid argument {FPS=}. Must be a number.')
+       exit(-1)
 
     logger.debug(f'creating groundlight client with {ENDPOINT=} and {TOKEN=}')
     gl = Groundlight(endpoint=ENDPOINT, api_token=TOKEN)
 
     logger.debug(f'initializing video capture: {STREAM=}')
-    cap = cv2.VideoCapture(STREAM, cv2.CAP_FFMPEG)
+    cap = cv2.VideoCapture(STREAM, cv2.CAP_ANY)
 
+    q = Queue()
+    workers = []
+    for i in range(round(FPS)):
+       thread = Thread(target=frame_processor, kwargs=dict(q=q, client=gl, detector=DETECTOR))
+       workers.append(thread)
+       thread.start()
+
+    desired_delay = 1/FPS
+    start = time.time()
     while True:
-       start = time.time()
        if not cap.isOpened():
            logger.error(f'Cannot open stream {STREAM=}')
-           exit(-1)
-
+           exit(1)
        ret, frame = cap.read()
-       logger.debug(f"Original {frame.shape=}")
-       frame = cv2.resize(frame, (480,270))
-       logger.debug(f"Resized {frame.shape=}")
-
-       is_success, buffer = cv2.imencode(".jpg", frame)
-       logger.debug(f"buffer size is {len(buffer)}")
-       io_buf = io.BytesIO(buffer)
-       end = time.time()
-       logger.info(f"Time to prep image {1000*(end-start):.1f}ms")
-       image_query = gl.submit_image_query(detector_id=DETECTOR, image=io_buf)
-       start = end
-       end = time.time()
-       logger.info(f"API time for image {1000*(start-end):.1f}ms")
+       now = time.time()
+       logger.info(f'captured a frame after {now-start}.')
+       start = now
+       logger.debug(f'read() -> {ret=}, {frame=}')
+       if not ret:
+          logger.warning(f'continuing because {ret=}')
+          continue
+       q.put(frame)
+       now = time.time()
+       actual_delay = desired_delay - (now-start)
+       logger.debug(f'waiting for {actual_delay=} to capture the next frame.')
+       if actual_delay < 0:
+          logger.warning(f'Falling behind the desired {FPS=}! looks like putting frames into the worker queue is taking too long: {now-start}s. The queue contains {len(q)} frames.')
+          actual_delay = 0
+       time.sleep(actual_delay)
 
     cap.release()
 
