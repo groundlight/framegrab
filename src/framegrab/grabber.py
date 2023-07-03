5a3d16844f4e199f2b2ad9ddcc2c1b862dbde33a
timmarkhuff <timmarkhuff@gmail.com>
2023-07-03 12:12:39 -0700

    auto enumeration of webcams

--- src/framegrab/grabber.py
+++ src/framegrab/grabber.py
@@ -19,7 +19,6 @@ from typing import List
 
 logger = logging.getLogger(__name__)
 
-
 class InputTypes(Enum):
     WEBCAM = 'webcam'
     RTSP = 'rtsp'
@@ -53,7 +52,16 @@ class FrameGrabber(ABC):
         elif input_type == InputTypes.YOUTUBE.value:
             return None
         else:
-            raise ValueError(f'Unable to determine input_type. Valid types are {[i.value for i in InputTypes]}')
+            raise ValueError(
+                f'Unable to determine input_type. Valid types are {[i.value for i in InputTypes]}'
+                )
+
+    @staticmethod
+    def autodiscover() -> dict:
+        """just a stub
+        autodiscovers cameras and returns a dictionary of grabbers
+        """
+        return {}
 
     def _postprocess(self, frame: np.ndarray) -> np.ndarray:
         # just return in the input if there are no postprocessing options provided.
@@ -71,13 +79,171 @@ class FrameGrabber(ABC):
         return frame
 
     @abstractmethod
-    def read():
+    def read() -> np.ndarray:
         pass
 
     @abstractmethod
-    def release():
+    def release() -> None:
         pass
 
+    @abstractmethod
+    def _apply_options() -> None:
+        pass
+
+class WebcamFrameGrabber(FrameGrabber):
+    indices_in_use = set()
+
+    def __init__(self, config: dict):
+        self.config = config
+        self.idx = None
+
+        # assign cameras based on serial number
+        if self.config.get('serial_number', None):
+            found_webcam_devnames = WebcamFrameGrabber._find_webcam_devnames()
+            desired_serial_number = self.config.get('serial_number', None)
+            for devname, serial_number in found_webcam_devnames.items():
+                if serial_number == desired_serial_number:
+                    self.capture = cv2.VideoCapture(devname)
+                    break
+            else:
+                raise Exception(
+                    f'Unable to find webcam with the specified serial_number: {desired_serial_number}. '
+                    'Please ensure that the serial number is correct and that it is plugged in.'
+                    )
+        # since no serial number is provided, just assign the next available camera
+        else:
+            for idx in range(20):
+                if idx in WebcamFrameGrabber.indices_in_use:
+                    logger.debug(f'Webcam index {idx} already in use.')
+                    continue
+
+                capture = cv2.VideoCapture(idx)
+                ret, _ = capture.read()
+                if ret and capture.isOpened():
+                    logger.info(f'Connected to webcam index {idx}.')
+                    self.capture = capture
+                    self.idx = idx
+                    WebcamFrameGrabber.indices_in_use.add(idx)
+                    break
+            else:
+                raise Exception(
+                    'Unable to connect to webcam by index. Is your webcam plugged in?'
+                )
+
+        # set the buffer size to 1 to always get the most recent frame
+        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        
+    def read(self) -> np.ndarray:
+        ret, frame = self.capture.read()
+        frame = self._postprocess(frame)
+        frame = self._apply_options(frame)
+        return frame
+
+    def release(self) -> None:
+        self.capture.release()
+        if self.idx:
+            WebcamFrameGrabber.indices_in_use.remove(self.idx)
+
+    def _apply_options(self, frame: np.ndarray) -> np.ndarray:
+        """just a stub
+        should apply can camera specific options to a frame and return the frame
+        """
+        return frame
+
+    @staticmethod
+    def _find_webcam_devnames() -> dict: 
+        """Finds all plugged in webcams and returns a dictionary mapping device names to
+        to serial numbers. This is useful for connecting the dots between user provided configurations
+        and actual plugged in devices.
+
+        This function only works on Linux, and was specifically tested on an Nvidia Jetson.
+        """
+        start = time.time()
+        # ls /dev/video* returns device paths for all plugged in webcams
+        command = 'ls /dev/video*'
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        stdout, _ = process.communicate()
+        output = stdout.decode('utf-8')
+        devices = output.strip().split('\n')
+
+        plugged_in_devices = {}
+        for devpath in devices:
+            # ls -l /sys/class/video4linux/video0/device returns a path that points back into the /sys/bus/usb/devices/
+            # directory where can determine the serial number.
+            # e.g. /sys/bus/usb/devices/2-3.2:1.0 -> /sys/bus/usb/devices/<bus>-<port>.<subport>:<config>.<interface>
+            devname = devpath.split('/')[-1]
+            command = f'ls -l /sys/class/video4linux/{devname}/device'
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, _ = process.communicate()
+            output = stdout.decode('utf-8')
+            bus_port_subport = output.split('/')[-1].split(':')[0]
+
+            # find the serial number
+            command = f'cat /sys/bus/usb/devices/{bus_port_subport}/serial'
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            stdout, _ = process.communicate()
+            serial_number = stdout.decode('utf-8').strip()
+
+            if serial_number:
+                plugged_in_devices[devpath] = serial_number
+
+        stop = time.time()
+        time_elapsed = stop - start
+        print(f'_find_webcam_devnames ran in {time_elapsed} seconds')
+
+        return plugged_in_devices
+
+class RTSPFrameGrabber(FrameGrabber):
+    """grabs the most recent frame from an rtsp stream. The RTSP capture
+    object has a non-configurable built-in buffer, so just calling
+    grab would return the oldest frame in the buffer rather than the
+    latest frame. This class uses a thread to continously drain the
+    buffer by grabbing and discarding frames and only returning the
+    latest frame when explicitly requested.
+    """
+
+    def __init__(self, config: dict):
+        self.config = config
+
+        stream = config['address']
+        self.capture = cv2.VideoCapture(stream)
+
+        logger.debug(f"initialized video capture with backend={self.capture.getBackendName()}")
+        if not self.capture.isOpened():
+            raise ValueError(f"could not open RTSP stream: {stream}")
+        
+        self.run = True
+        self.lock = Lock()
+        Thread(target=self._drain).start()
+
+    def read(self) -> np.ndarray:
+        start = time.time()
+        with self.lock:
+            logger.debug(f"grabbed lock to read frame from buffer")
+            ret, frame = self.capture.read()  # grab and decode since we want this frame
+            if not ret:
+                logger.error(f"could not read frame from {self.capture}")
+            now = time.time()
+            logger.debug(f"read the frame in {1000*(now-start):.1f}ms")
+
+            frame = self._postprocess(frame)
+
+            return frame
+
+    def release(self) -> None:
+        self.run = False # to stop the buffer drain thread
+
+    def _apply_options() -> None:
+        pass
+
+    def _drain(self) -> None:
+        logger.debug(f"starting thread to drain the video capture buffer")
+        while self.run:
+            with self.lock:
+                ret = self.capture.grab()  # just grab and don't decode
+
+        # release the stream on the way out
+        self.capture.release()
 
 class DirectoryFrameGrabber(FrameGrabber):
     def __init__(self, stream=None, fps_target=0):
@@ -166,7 +332,7 @@ class DeviceFrameGrabber(FrameGrabber):
             )
             raise e
 
-    def grab(self):
+    def grab(self) -> np.ndarray:
         """consistent with existing behavior based on VideoCapture.read()
         which may return None when it cannot read a frame.
         """
@@ -177,119 +343,6 @@ class DeviceFrameGrabber(FrameGrabber):
         now = time.time()
         logger.debug(f"read the frame in {1000*(now-start):.1f}ms")
         return frame
-
-class WebcamFrameGrabber(FrameGrabber):
-    capture_idx = 0
-
-    def __init__(self, config: dict):
-        self.config = config
-
-        found_webcam_serial_numbers = WebcamFrameGrabber._find_webcam_serial_numbers()
-        print(f'found_webcam_serial_numbers: {found_webcam_serial_numbers}')
-        desired_serial_number = self.config.get('serial_number', None)
-        for serial_number, devname in found_webcam_serial_numbers.items():
-            if serial_number == desired_serial_number:
-                self.capture = cv2.VideoCapture(devname)
-                self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-                break
-        else:
-            logger.warning(
-                f'Unable to find webcam with the specified serial_number: {desired_serial_number}. '
-                'Is it plugged in?'
-                )
-
-    def read(self) -> np.ndarray:
-        ret, frame = self.capture.read()
-        frame = self._postprocess(frame)
-        return frame
-
-    def release(self) -> None:
-        self.capture.release()
-
-    @staticmethod
-    def _find_webcam_serial_numbers() -> dict: 
-        """Finds all plugged in webcams and returns a dictionary where the keys are serial numbers
-        and the values are device paths that can be passed to OpenCV to initialize a VideoCapture.
-        This function only works on Linux, and was specifically tested on an Nvidia Jetson.
-        """
-
-        # ls /dev/video* returns device paths for all plugged in webcams
-        command = 'ls /dev/video*'
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        stdout, _ = process.communicate()
-        output = stdout.decode('utf-8')
-        devices = output.strip().split('\n')
-
-        plugged_in_devices = {}
-        for devpath in devices:
-            # ls -l /sys/class/video4linux/video0/device returns a path that points back into the /sys/bus/usb/devices/
-            # directory where can determine the serial number.
-            # e.g. /sys/bus/usb/devices/2-3.2:1.0 -> /sys/bus/usb/devices/<bus>-<port>.<subport>:<config>.<interface>
-            devname = devpath.split('/')[-1]
-            command = f'ls -l /sys/class/video4linux/{devname}/device'
-            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            stdout, _ = process.communicate()
-            output = stdout.decode('utf-8')
-            bus_port_subport = output.split('/')[-1].split(':')[0]
-
-            # find the serial number
-            command = f'cat /sys/bus/usb/devices/{bus_port_subport}/serial'
-            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            stdout, _ = process.communicate()
-            serial_number = stdout.decode('utf-8').strip()
-
-            if serial_number:
-                plugged_in_devices[serial_number] = devpath
-
-        return plugged_in_devices
-
-class RTSPFrameGrabber(FrameGrabber):
-    """grabs the most recent frame from an rtsp stream. The RTSP capture
-    object has a non-configurable built-in buffer, so just calling
-    grab would return the oldest frame in the buffer rather than the
-    latest frame. This class uses a thread to continously drain the
-    buffer by grabbing and discarding frames and only returning the
-    latest frame when explicitly requested.
-    """
-
-    def __init__(self, config: dict):
-        self.config = config
-
-        stream = config['address']
-        self.capture = cv2.VideoCapture(stream)
-
-        logger.debug(f"initialized video capture with backend={self.capture.getBackendName()}")
-        if not self.capture.isOpened():
-            raise ValueError(f"could not open RTSP stream: {stream}")
-        
-        self.run = True
-        self.lock = Lock()
-        self.thread = Thread(target=self._drain)
-        self.thread.start()
-
-    def read(self):
-        start = time.time()
-        with self.lock:
-            logger.debug(f"grabbed lock to read frame from buffer")
-            ret, frame = self.capture.read()  # grab and decode since we want this frame
-            if not ret:
-                logger.error(f"could not read frame from {self.capture}")
-            now = time.time()
-            logger.debug(f"read the frame in {1000*(now-start):.1f}ms")
-
-            frame = self._postprocess(frame)
-
-            return frame
-
-    def release(self):
-        self.run = False # to stop the buffer drain thread
-        self.capture.release()
-
-    def _drain(self):
-        logger.debug(f"starting thread to drain the video capture buffer")
-        while self.run:
-            with self.lock:
-                ret = self.capture.grab()  # just grab and don't decode
 
 class YouTubeFrameGrabber(FrameGrabber):
     """grabs the most recent frame from an YouTube stream. To avoid extraneous bandwidth
