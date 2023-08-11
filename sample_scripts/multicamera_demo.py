9c798f59c31477ebc9fd40c9ef0ef19143988e52
Leo Dirac <leo@positronix.ai>
2023-08-11 13:14:05 -0700

    Changing sample config to use direct yaml instead of yaml-in-string.

--- sample_scripts/multicamera_demo.py
+++ sample_scripts/multicamera_demo.py
@@ -7,8 +7,7 @@ import cv2
 # load the configurations from yaml
 config_path = 'sample_config.yaml'
 with open(config_path, 'r') as f:
-    data = yaml.safe_load(f)
-    configs = yaml.safe_load(data['GL_CAMERAS'])
+    configs = yaml.safe_load(f)['cameras']
 
 print('Loaded the following configurations from yaml:')
 print(configs)
