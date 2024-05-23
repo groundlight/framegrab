8d31a54173065b8563d054c15c69bcc9f5b6fa49
Tim Huff <89954856+timmarkhuff@users.noreply.github.com>
2024-05-23 11:29:58 -0700

    Fix for IR cameras (#39)
    * first commit
    
    * debugging image color
    
    * more greyscale debugging
    
    * adjusting greyscale function
    
    * updating comments
    
    * Automatically reformatting code with black and isort
    
    * adjusting some logging
    
    * Automatically reformatting code with black and isort
    
    * adding special handling for the Logitech Brio
    
    * Automatically reformatting code with black and isort
    
    * debugging
    
    * Automatically reformatting code with black and isort
    
    * debugging
    
    * Automatically reformatting code with black and isort
    
    * fixing a bug
    
    * fixing camera open issue
    
    * Automatically reformatting code with black and isort
    
    * bumping version and cleaning up logging
    
    * Automatically reformatting code with black and isort
    
    * responding to PR feedback
    
    ---------
    
    Co-authored-by: Tim Huff <tim@groundlight.ai>
    Co-authored-by: Auto-format Bot <autoformatbot@groundlight.ai>

--- src/framegrab/grabber.py
+++ src/framegrab/grabber.py
@@ -6,7 +6,7 @@ import subprocess
 import time
 from abc import ABC, abstractmethod
 from threading import Lock, Thread
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Union
 from urllib.parse import urlparse
 
 import cv2
@@ -512,36 +512,36 @@ class GenericUSBFrameGrabber(FrameGrabber):
 
         # Assign camera based on serial number if 1) serial_number was provided and 2) we know the
         # serial numbers of plugged in devices
-        if serial_number and found_cams:
+        if found_cams:
+            logger.debug(f"Found {len(found_cams)} USB cameras with Linux commands. Assigning camera by serial number.")
             for found_cam in found_cams:
-                if serial_number != found_cam["serial_number"]:
+                if serial_number and serial_number != found_cam["serial_number"]:
                     continue
 
                 idx = found_cam["idx"]
                 if idx in GenericUSBFrameGrabber.indices_in_use:
-                    raise ValueError(
-                        f"USB camera index {idx} already in use. "
-                        f"Did you use the same serial number ({serial_number}) for two different cameras?"
-                    )
+                    continue
 
-                capture = cv2.VideoCapture(idx)
-                if capture.isOpened():
-                    break  # Found a valid capture, no need to look any further
+                capture = self._connect_and_validate_capture(found_cam)
+                if capture is not None:
+                    break  # Found a valid capture
 
             else:
                 raise ValueError(
                     f"Unable to find USB camera with the specified serial_number: {serial_number}. "
-                    "Please ensure that the serial number is correct and that the camera is plugged in."
+                    "Please ensure that the serial number is correct, that the camera is plugged in, and that the camera is not already in use."
                 )
-        # If no serial number was provided, just assign the next available camera by index
+        # If we don't know the serial numbers of the cameras, just assign the next available camera by index
         else:
+            logger.debug("No USB cameras found with Linux commands. Assigning camera by index.")
             for idx in range(20):  # an arbitrarily high number to make sure we check for enough cams
                 if idx in GenericUSBFrameGrabber.indices_in_use:
                     continue  # Camera is already in use, moving on
 
                 capture = cv2.VideoCapture(idx)
                 if capture.isOpened():
-                    break  # Found a valid capture, no need to look any further
+                    break  # Found a valid capture
+
             else:
                 raise ValueError("Unable to connect to USB camera by index. Is your camera plugged in?")
 
@@ -559,7 +559,60 @@ class GenericUSBFrameGrabber(FrameGrabber):
         self.idx = idx
         GenericUSBFrameGrabber.indices_in_use.add(idx)
 
+    def _has_ir_camera(self, camera_name: str) -> bool:
+        """Check if the device contains an IR camera.
+
+        Cameras such as the Logitech Brio contain an infrared camera for unlocking the computer with features
+        such as Windows Hello. These cameras are not suitable for use in the context of most applications, so we will
+        exclude them.
+        """
+        cameras_with_ir = ["logitech brio"]  # we can add to this list as we discover more cameras with IR
+        for i in cameras_with_ir:
+            if i in camera_name.lower():
+                return True
+        return False
+
+    def _connect_and_validate_capture(self, camera_details: Dict[str, str]) -> Union[cv2.VideoCapture, None]:
+        """Connect to the camera, check that it is open and not an IR camera.
+
+        Return the camera if it is valid, otherwise return None.
+        """
+        idx = camera_details["idx"]
+        camera_name = camera_details["camera_name"]
+        device_path = camera_details["device_path"]
+
+        capture = cv2.VideoCapture(idx)
+        if not capture.isOpened():
+            logger.warning(f"Could not open camera with index {idx}")
+            return None
+
+        has_ir_camera = self._has_ir_camera(camera_name)
+        if has_ir_camera:
+            ret, frame = capture.read()
+            if not ret:
+                logger.warning(f"Could not read frame from {device_path}")
+                return None
+            elif self._is_grayscale(frame):
+                logger.info(
+                    f"This device ({device_path}) is grayscale and therefore likely an IR camera. Skipping this camera."
+                )
+                capture.release()
+                return None
+            else:
+                return capture
+        else:
+            return capture
+
+    def _is_grayscale(self, frame: np.ndarray) -> bool:
+        """Check if the provided frame is grayscale."""
+        b, g, r = cv2.split(frame)
+
+        return np.array_equal(b, g) and np.array_equal(g, r)
+
     def _grab_implementation(self) -> np.ndarray:
+        if not self.capture.isOpened():
+            self.capture.open(self.idx)
+
         # OpenCV VideoCapture buffers frames by default. It's usually not possible to turn buffering off.
         # Buffer can be set as low as 1, but even still, if we simply read once, we will get the buffered (stale) frame.
         # Assuming buffer size of 1, we need to read twice to get the current frame.
@@ -579,6 +632,13 @@ class GenericUSBFrameGrabber(FrameGrabber):
         self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
     @staticmethod
+    def _run_system_command(command: str) -> str:
+        """Runs a Linux system command and returns the stdout as a string."""
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        stdout, _ = process.communicate()
+        return stdout.decode("utf-8").strip()
+
+    @staticmethod
     def _find_cameras() -> list:
         """Attempts to finds all USB cameras and returns a list dictionaries, each dictionary containing
         information about a camera, including: serial_number, device name, index, etc.
@@ -590,14 +650,12 @@ class GenericUSBFrameGrabber(FrameGrabber):
 
         # ls /dev/video* returns device paths for all detected cameras
         command = "ls /dev/video*"
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        stdout, _ = process.communicate()
+        output = GenericUSBFrameGrabber._run_system_command(command)
 
-        if len(stdout) == 0:  # len is zero when no cameras are plugged in
+        if len(output) == 0:  # len is zero when no cameras are plugged in
             device_paths = []
         else:
-            output = stdout.decode("utf-8")
-            device_paths = output.strip().split("\n")
+            device_paths = output.split("\n")
 
         found_cams = []
         for device_path in device_paths:
@@ -606,16 +664,16 @@ class GenericUSBFrameGrabber(FrameGrabber):
             # e.g. /sys/bus/usb/devices/2-3.2:1.0 -> /sys/bus/usb/devices/<bus>-<port>.<subport>:<config>.<interface>
             devname = device_path.split("/")[-1]
             command = f"ls -l /sys/class/video4linux/{devname}/device"
-            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            stdout, _ = process.communicate()
-            output = stdout.decode("utf-8")
+            output = GenericUSBFrameGrabber._run_system_command(command)
             bus_port_subport = output.split("/")[-1].split(":")[0]
 
             # find the serial number
             command = f"cat /sys/bus/usb/devices/{bus_port_subport}/serial"
-            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            stdout, _ = process.communicate()
-            serial_number = stdout.decode("utf-8").strip()
+            serial_number = GenericUSBFrameGrabber._run_system_command(command)
+
+            # find the camera name (e.g. Logitech Brio)
+            command = f"cat /sys/class/video4linux/{devname}/name"
+            camera_name = GenericUSBFrameGrabber._run_system_command(command)
 
             # find the index
             idx = int(re.findall(r"\d+", devname)[-1])
@@ -626,6 +684,7 @@ class GenericUSBFrameGrabber(FrameGrabber):
                         "serial_number": serial_number,
                         "device_path": device_path,
                         "idx": idx,
+                        "camera_name": camera_name,
                     }
                 )
 
