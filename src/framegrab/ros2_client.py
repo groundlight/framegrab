58e7012776533e8c1f526e56778b682abc25ea4c
Tim Huff <timmarkhuff@gmail.com>
2025-05-07 15:10:03 -0700

    more improvements to robustness of topic discovery

--- src/framegrab/ros2_client.py
+++ src/framegrab/ros2_client.py
@@ -17,8 +17,13 @@ SUPPORTED_IMAGE_TYPES = (
 
 
 def _topic_discovery_retry(
-    func: Callable[[], List[Tuple[str, List[str]]]], attempts: int = 5, delay: float = 0.01
+    func: Callable[[], List[Tuple[str, List[str]]]], attempts: int = 10, delay: float = 0.01
 ) -> List[Tuple[str, List[str]]]:
+    """
+    Topic discovery can be flakey in ROS. 
+    
+    Wrap topic discovery (Node.get_topic_names_and_types) in this function to get more robust results.
+    """
 
     topic_dict: Dict[str, set] = {}
 
