31f828c8e89c4305a1e39c8dca5df37b9494fd8e
Tim Huff <timmarkhuff@gmail.com>
2025-12-03 14:28:06 -0800

    adding a test

---
+++ test/test_ascii_magic_dependency.py
@@ -0,0 +1,20 @@
+import unittest
+
+import ascii_magic
+from PIL import Image
+
+
+class TestAsciiMagicDependency(unittest.TestCase):
+    def test_from_pillow_image_produces_ascii_art(self):
+        image = Image.new("RGB", (32, 32), color="black")
+        art = ascii_magic.from_pillow_image(image)
+
+        self.assertIsInstance(art, ascii_magic.AsciiArt)
+
+        ascii_output = art.to_ascii(columns=16)
+        self.assertTrue(ascii_output.strip())
+
+
+if __name__ == "__main__":
+    unittest.main()
+
