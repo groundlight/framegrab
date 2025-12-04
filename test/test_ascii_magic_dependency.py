8ee65bcd2fd884d97340f09f3ad76afd314c14c1
Tim Huff <timmarkhuff@gmail.com>
2025-12-03 16:23:14 -0800

    removing an unnecessary test

--- test/test_ascii_magic_dependency.py
+++ test/test_ascii_magic_dependency.py
@@ -1,15 +1,15 @@
-import unittest
+# import unittest
 
-import ascii_magic
-from PIL import Image
+# import ascii_magic
+# from PIL import Image
 
 
-class TestAsciiMagicDependency(unittest.TestCase):
-    def test_from_pillow_image_produces_ascii_art(self):
-        image = Image.new("RGB", (32, 32), color="black")
-        art = ascii_magic.from_pillow_image(image)
+# class TestAsciiMagicDependency(unittest.TestCase):
+#     def test_from_pillow_image_produces_ascii_art(self):
+#         image = Image.new("RGB", (32, 32), color="black")
+#         art = ascii_magic.from_pillow_image(image)
 
-        self.assertIsInstance(art, ascii_magic.AsciiArt)
+#         self.assertIsInstance(art, ascii_magic.AsciiArt)
 
-        ascii_output = art.to_ascii(columns=16)
-        self.assertTrue(ascii_output.strip())
+#         ascii_output = art.to_ascii(columns=16)
+#         self.assertTrue(ascii_output.strip())
